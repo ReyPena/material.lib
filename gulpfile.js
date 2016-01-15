7b543bbc9b164e15f78c12da9175c45c18db8978
ReyPena <zonickblack@gmail.com>
2016-01-15 12:36:07 -0700

    Gulpfile start and little modifications on colors file.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require("gulp")
+  , concat = require("gulp-concat")
+  , uglify =  require("gulp-uglify")
+  , uglifyCss = require("gulp-uglifycss")
+  , ngAnnotate =  require("gulp-ng-annotate")
+  , watch = require("gulp-watch");
+
+var paths = {
+
+};
+
+gulp.task();
+
+gulp.task("default", []);
