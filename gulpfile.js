0e2cbc8ef40190c56a50b1ce6d8a34456bfeace4
ReyPena <zonickblack@gmail.com>
2016-01-18 13:48:52 -0700

    Add some content to the gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,30 @@ var gulp = require("gulp")
   , concat = require("gulp-concat")
   , uglify =  require("gulp-uglify")
   , uglifyCss = require("gulp-uglifycss")
-  , ngAnnotate =  require("gulp-ng-annotate")
+  , sass = require("gulp-sass")
   , watch = require("gulp-watch");
 
 var paths = {
-
+  scss: "./scss/*.scss",
+  // components:
 };
 
-gulp.task();
+gulp.task("cssLib", function () {
+  gulp.src(path.scss)
+    .pipe(sass())
+    .pipe(concat("./dis/Csslib.css"));
+});
+
+gulp.task("cssLibMin", function () {
+  gulp.src(path.scss)
+    .pipe(sass())
+    .pipe(uglifyCss())
+    .pipe(concat("./dis/miniCsslib.css"));
+});
+
+gulp.task("watch", function () {
+  gulp.watch(paths.scss, ["cssLib", "cssLibMin"]);
+  gulp.watch(paths.components, ["cssLib", "cssLibMin"]);
+});
 
 gulp.task("default", []);
